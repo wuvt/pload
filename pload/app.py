f10a82a5064ecda0aa12269d46eaf3760d388343
mutantmonkey <mutantmonkey@mutantmonkey.in>
2020-07-04 18:16:38 -0700

    Add import-songs CLI command

--- pload/app.py
+++ pload/app.py
@@ -1,6 +1,9 @@
 import base64
+import click
+import json
 import os
 import urllib.parse
+import uuid
 from flask import Flask
 from .views import bp
 from .db import db, init_db, migrate
@@ -93,3 +96,14 @@ app = create_app()
 def initdb():
     with app.app_context():
         init_db()
+
+
+@app.cli.command()
+@click.option("--json-path")
+def import_songs(json_path):
+    with app.app_context():
+        es.delete_by_query("songs", body={"query": {"term": {"url": "*",},},})
+
+        data = json.load(open(json_path))
+        for entry in data:
+            es.create("songs", uuid.uuid4(), body=entry)
