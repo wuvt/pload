6befab7c9a0333af7a71a2cd687d5c082b9e51cc
mutantmonkey <mutantmonkey@mutantmonkey.in>
2020-07-20 22:01:27 -0700

    Add wuvtLinx regex rewrites to defaults.py
    Although the lack of a rewrite here is what allowed us to identify #40
    so quickly, the lack of a rewrite for this violates the principle of
    least surprise.

--- pload/defaults.py
+++ pload/defaults.py
@@ -15,8 +15,10 @@ TRACKMAN_URL = "https://trackman-fm.apps.wuvt.vt.edu/"
 TRACK_VALIDATE_CHECK_EXISTS = True
 TRACK_URL_REWRITES = [
     (r"^https:\/\/files\.apps\.wuvt\.vt\.edu", "http://alexandria.wuvt.vt.edu"),
+    (r"^https:\/\/linx\.apps\.wuvt\.vt\.edu(\/[^\/]+)$", "https://linx.apps.wuvt.vt.edu/selif\\1"),
 ]
 TRACK_URL_DISPLAY_REWRITES = [
     (r"^http:\/\/alexandria\.wuvt\.vt\.edu", "https://files.apps.wuvt.vt.edu"),
     (r"^http:\/\/192\.168\.0\.25", "https://files.apps.wuvt.vt.edu"),
+    (r"^https:\/\/linx\.apps\.wuvt\.vt\.edu\/selif\/", "https://linx.apps.wuvt.vt.edu/"),
 ]
